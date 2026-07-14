f1dc665beb32ab157e12d84b1c22831a687d4c62
yasuda504883-ship-it <yasuda504883@gmail.com>
2026-07-14 16:25:27 +0900

    Add missing STAR GUYS store areas

---
+++ stores-extra.js
@@ -0,0 +1,61 @@
+window.STORE_GROUPS = window.STORE_GROUPS || {};
+
+Object.assign(window.STORE_GROUPS, {
+  "岡山": `
+ARTEL
+S-GRAND
+S-cort
+STNY
+SHINY -1st-
+SHINY -本店-
+SHANTI
+SUGAR
+GENESIS
+SUITE sinphonia
+SPARK
+SEXYY
+SØL
+SOWARE
+DIAVEL
+`,
+  "石川／金沢": `
+ACQUA -KANAZAWA-
+alpha KANAZAWA by ACQUA
+`,
+  "香川／高松": `
+ADAM
+EST
+EMPEROR
+Destiny -acro-
+Roots
+`,
+  "愛媛／松山": `
+IRIS
+Alive -after force-
+ART
+NEXAS
+WHITE
+Real
+`,
+  "徳島": `
+ALICE
+鳳
+Re:EDEN
+Romeo Plus
+`,
+  "すすきの": `
+GENTLY BLACK
+Mr.sapporo
+YGGDRASILL -SAPPORO-
+`,
+  "熊本": `
+Dear's熊本
+`,
+  "鹿児島": `
+Miracle Candy
+`,
+  "沖縄／那覇": `
+Storme
+Mr.A
+`,
+});
